eaf3bc054b42b0dbc9c333694a21baf2267ca158
Vladimir Galović <vgalovic99@gmail.com>
2024-12-17 20:18:24 +0100

    Rename sequential_circuit to latches_and_flip-flops

--- VHDL/latches_and_flip-flops/setup.tcl
+++ VHDL/latches_and_flip-flops/setup.tcl
@@ -2,7 +2,7 @@
 set target_hdl "VHDL"
 
 # Define project name
-set project_name "sequential_circuit"
+set project_name "latches_and_flip-flops"
 
 # Set project directory
 set create_project_dir [file normalize [file join [file dirname [info script]]  "../.."]]
